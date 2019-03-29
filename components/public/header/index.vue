cb4e6bff4d395e2d96ef05c116bf5f19dbaf2e1b
HelenXu <HelenXu@openrice.com>
2019-03-29 10:26:03 +0800

    commit

---
+++ components/public/header/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-row :gutter="15" class="m-header">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  </el-row>
+</template>
+<script>
+import Geo from './geo.vue'
+export default {
+  components:{
+    Geo
+  }
+}
+</script>
+<style lang="scss">
+  @import "assets/css/public/layout.scss";
+</style>
+
+
